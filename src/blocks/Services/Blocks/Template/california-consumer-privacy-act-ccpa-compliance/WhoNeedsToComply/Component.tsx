8ab4169bb770a8271636cd28aa376d37c4d68ee2
Prakhar Katiyar <Prakhar.katiyar@ensuesoft.com>
2025-09-26 00:16:10 +0530

    heelo

--- src/blocks/Services/Blocks/Template/california-consumer-privacy-act-ccpa-compliance/WhoNeedsToComply/Component.tsx
+++ src/blocks/Services/Blocks/Template/california-consumer-privacy-act-ccpa-compliance/WhoNeedsToComply/Component.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 interface Props {
-  data: {
-    title: string;
-    description: string;
-    items: { number: string; text: string }[];
-    backgroundImage: { url: string };
+  data?: {
+    title?: string;
+    description?: string;
+    items?: { number?: string; text?: string }[];
+    backgroundImage?: { url?: string };
   };
 }
 
 const WhoNeedsToComplyComponent: React.FC<Props> = ({ data }) => {
+  if (!data) return null;
   return (
     <section
       className={`bg-[url("${data?.backgroundImage?.url || ""}")] bg-no-repeat bg-cover bg-left sm:bg-center before:bg-black/50 before:absolute before:top-0 before:left-0 before:w-full before:h-full relative`}
@@ -20,23 +21,25 @@ const WhoNeedsToComplyComponent: React.FC<Props> = ({ data }) => {
 
           <div className="sm:col-span-9 lg:col-span-6">
             <h2 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-4 lg:mb-6">
-               {data?.title || "Who needs to comply?"}
+              {data?.title || "Who needs to comply?"}
             </h2>
 
-            <p className="text-white">{data.description}</p>
+            {data.description && <p className="text-white">{data.description}</p>}
 
-            <div className="mt-8">
-              <ul className="flex flex-col gap-8 before:w-0.5 before:h-10/12 before:bg-white before:absolute before:left-6 before:-z-10 before:top-1/2 before:-translate-y-1/2 relative">
-                {data.items.map((item, index) => (
-                  <li key={index} className="flex items-center gap-4 text-white">
-                    <div className="bg-(--wils_orang) rounded-full p-4 font-bold size-[50px] flex items-center justify-center">
-                      {item.number}
-                    </div>
-                    <p dangerouslySetInnerHTML={{ __html: item.text }} />
-                  </li>
-                ))}
-              </ul>
-            </div>
+            {Array.isArray(data.items) && (
+              <div className="mt-8">
+                <ul className="flex flex-col gap-8 before:w-0.5 before:h-10/12 before:bg-white before:absolute before:left-6 before:-z-10 before:top-1/2 before:-translate-y-1/2 relative">
+                  {data.items.map((item, index) => (
+                    <li key={index} className="flex items-center gap-4 text-white">
+                      <div className="bg-(--wils_orang) rounded-full p-4 font-bold size-[50px] flex items-center justify-center">
+                        {item?.number}
+                      </div>
+                      <p dangerouslySetInnerHTML={{ __html: item?.text || "" }} />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
